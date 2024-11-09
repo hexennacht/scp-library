8e90a68883600f227313bf6f7d1d9745687dbbf5
Taufik R Firdaus <taufik_ramadan@hotmail.com>
2024-11-09 20:58:53 +0700

    some modification and newer screenshoot

--- apps/scp-api/main.py
+++ apps/scp-api/main.py
@@ -4,12 +4,10 @@ from core.configuration import settings
 import uvicorn
 
 
-def main() -> FastAPI:
+def app() -> FastAPI:
     app = FastAPI(title=settings.app.name, version=settings.app.version)
     app.include_router(api)
     return app
-
-app = main()
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=settings.app.host, port=settings.app.port, reload=True)
