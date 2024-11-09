c978fa5c349b96b6d790607c25fcdf6d2f583d9b
Taufik R. Firdaus <taufik_ramadan@hotmail.com>
2024-11-09 20:17:44 +0700

    add delete and update and avoid sql injection (#2)
    * add delete and update and avoid sql injection
    
    * fixing more query

--- apps/scp-api/services/impl/category.py
+++ apps/scp-api/services/impl/category.py
@@ -1,3 +1,4 @@
+import logging
 from models.base import FilterParams
 from typing import Tuple
 from services.category import CategoryService
@@ -5,7 +6,11 @@ from models.category import Category
 from psycopg2 import ProgrammingError
 from fastapi import HTTPException
 from sqlalchemy import text, Engine
-from core.database import engine
+from datetime import datetime
+
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 class CategoryServiceImpl(CategoryService):
     db: Engine
@@ -14,8 +19,6 @@ class CategoryServiceImpl(CategoryService):
         self.db = db_engine
 
     async def get_categories(self, param: FilterParams) -> Tuple:
-        print(self.db is None)
-
         with self.db.connect() as connection:
             try:
                 count = int(f"{connection.execute(text("select count(id) from categories")).scalar()}")
@@ -37,8 +40,10 @@ class CategoryServiceImpl(CategoryService):
         with self.db.connect() as connection:
             trx = connection.begin()
             try:
+                args = {"name": category.name, "description": category.description}
                 id = trx.connection.execute(
-                        text(f"INSERT INTO categories (name, description) VALUES ('{category.name}', '{category.description}') RETURNING id")
+                        text(f"INSERT INTO categories (name, description) VALUES (:name, :description) RETURNING id"),
+                        args
                     ).scalar()
                 category.id = int(f"{id}")
                 trx.commit()
@@ -54,7 +59,8 @@ class CategoryServiceImpl(CategoryService):
             trx = connection.begin()
             try:
                 trx.connection.execute(
-                        text(f"DELETE FROM categories WHERE id = {id}")
+                        text(f"DELETE FROM categories WHERE id = :id"),
+                        {"id": id}
                     )
                 trx.commit()
             except ProgrammingError as e:
@@ -62,3 +68,48 @@ class CategoryServiceImpl(CategoryService):
                 raise HTTPException(status_code=500, detail=e.pgerror)
         trx.close()
         return True
+    
+    async def update(self, category: Category) -> Category:
+        old_category = await self.get_by_id(int(f"{category.id}"))
+
+        if old_category:
+            old_category.name = category.name
+            old_category.description = category.description
+            old_category.updated_at = datetime.now()
+
+            category = await self.update_by_id(old_category)
+
+        return category
+    
+
+    async def update_by_id(self, category: Category) -> Category:
+        with self.db.connect() as connection:
+            trx = connection.begin()
+            args = {"name": category.name, "description": category.description, "id": category.id}
+            try:
+                trx.connection.execute(
+                    text("UPDATE categories SET name = :name, description = :description WHERE id = :id"),
+                    args
+                )
+                trx.commit()
+            except ProgrammingError as e:
+                trx.rollback()
+                raise HTTPException(status_code=500, detail=e.pgerror)
+            
+        trx.close()
+
+        return category
+
+    async def get_by_id(self, id: int) -> Category:
+        with self.db.connect() as connection:
+            try:
+                result = connection.execute(text("SELECT * FROM categories WHERE id = :id"), {"id": id})
+            except ProgrammingError as e:
+                raise HTTPException(status_code=404, detail="CATEGORY NOT FOUND")
+        
+        row = result.fetchone()
+
+        if row is None:
+            raise HTTPException(status_code=404, detail="CATEGORY NOT FOUND")
+        
+        return Category(**row._mapping)
